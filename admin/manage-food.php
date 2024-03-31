67673b4203d7b75fd62aa367d21e6d9fb46b9eb8
ajaysth <shrestha11ajay@gmail.com>
2024-03-31 08:03:22 +0545

    changes

---
+++ admin/manage-food.php
@@ -0,0 +1,67 @@
+<?php include('partials/menu.php')?>
+
+
+
+<div class="main-content">
+    <div class="wrapper">
+    <h1>Manage Food</h1>
+    <br>
+
+<!-- button to add admin -->
+<a href="" class="btn-primary">Add Food</a>
+<br>
+<br>
+
+<table class="tbl-full">
+    <tr>
+        <th>S.N</th>
+        <th>Full Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+    </tr>
+
+    <tr>
+        <td>1.</td>
+        <td>Ajay Shrestha</td>
+        <td>ajayshrestha</td>
+        <td>
+            <a href="" class="btn-secondary">Update</a>
+            <a href="" class="btn-danger">Delete</a>
+
+        </td>
+    </tr>
+    <tr>
+        <td>2.</td>
+        <td>Sophiya Ghimire</td>
+        <td>sophiyaghimire</td>
+        <td>
+            <a href="" class="btn-secondary">Update</a>
+            <a href="" class="btn-danger">Delete</a>
+
+        </td>
+    </tr>
+    <tr>
+        <td>3.</td>
+        <td>Hola Culer</td>
+        <td>holaculer</td>
+        <td>
+            <a href="" class="btn-secondary">Update</a>
+            <a href="" class="btn-danger">Delete</a>
+
+        </td>
+    </tr>
+</table>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<?php include('partials/footer.php') ?>
