a4604aa07dde8102c6a636b5319a1c8f95d84800
ajaysth <shrestha11ajay@gmail.com>
2024-04-04 08:43:44 +0545

    delete and update file added and shown success or error messages with respective colors

--- admin/delete-admin.php
+++ admin/delete-admin.php
@@ -1,7 +1,34 @@
 <?php
+
+//include constants.php file here
+include('../config/constants.php');
 //   1.get the id of the admin to be deleted
+
+$id = $_GET['id'];
 
 
 // 2. create the query to delete the admin
 
-// 3. redirect to manage admin page with success message
+$sql = "DELETE FROM tbl_admin WHERE id=$id";
+
+//execute the query
+$res = mysqli_query($conn, $sql);
+
+//check whether sql query executed or not
+if ($res == TRUE) {
+    //deleted successfully
+    // echo "Admin deleted";
+    // create session variable to display message i
+    $_SESSION['delete'] = "<div class='success'>Admin deleted</div>";
+    // redirect to manage admin page
+    header('location:' . SITEURL . 'admin/manage-admin.php');
+} else {
+    //not deleted
+    // echo "not deleted";
+    $_SESSION['delete'] = "<div class='error'>Failed to delete admin</div>";
+    header('location:' . SITEURL . 'admin/manage-admin.php');
+}
+
+
+
+// 3. redirect to manage admin page with success/error message
--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,101 +1,99 @@
+<?php include('partials/menu.php') ?>
 
 
 
-<?php include('partials/menu.php')?>
+<!-- main content section starts -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Admin</h1>
+        <br>
 
+        <?php
+        if (isset($_SESSION['add'])) {
+            echo $_SESSION['add']; //displaying session messages
+            unset($_SESSION['add']); //removing session messages
+        }
 
+        if (isset($_SESSION['delete'])) {
+            echo $_SESSION['delete'];
+            unset($_SESSION['delete']);
+        }
+        ?>
+        <br>
+        <br>
 
-        <!-- main content section starts -->
-        <div class="main-content">
-        <div class="wrapper">
-                <h1>Manage Admin</h1>
-                <br>
+        <!-- button to add admin -->
+        <a href="add-admin.php" class="btn-primary">Add Admin</a>
+        <br>
+        <br>
 
-                <?php
-                if(isset($_SESSION['add']))
-                {
-                    echo $_SESSION['add']; //displaying session messages
-                    unset($_SESSION['add']);//removing session messages
+        <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <?php
+            //query to get all admin
+            $sql = "SELECT * FROM tbl_admin";
+
+            //execute the query
+            $res = mysqli_query($conn, $sql);
+
+            //check whether the query is executed or not
+            if ($res == TRUE) {
+                //COUNT rows to check whether there are data in database
+                $count = mysqli_num_rows($res);
+
+                $sn = 1; //create a variable and assign the value
+
+                //check number of rows
+                if ($count > 0) {
+                    //we have data in database
+                    while ($rows = mysqli_fetch_assoc($res)) {
+                        //using while loop to get all the data in database
+                        //it will run as long as we have data in database
+
+                        //get individual data
+                        $id = $rows['id'];
+                        $full_name = $rows['full_name'];
+                        $username = $rows['username'];
+
+                        //display values in table
+
+            ?>
+
+                        <tr>
+                            <td><?php echo $sn++ ?></td>
+                            <td><?php echo $full_name ?></td>
+                            <td><?php echo $username ?></td>
+                            <td>
+                                <a href="" class="btn-secondary">Update</a>
+                                <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete</a>
+
+                            </td>
+                        </tr>
+
+            <?php
+                    }
                 }
-                ?>
-                <br> 
-                <br>   
+            }
 
-                <!-- button to add admin -->
-                <a href="add-admin.php" class="btn-primary">Add Admin</a>
-                <br>
-                <br>
+            ?>
 
-                <table class="tbl-full">
-                    <tr>
-                        <th>S.N</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
-                        <th>Actions</th>
-                    </tr>
 
-                    <?php
-                    //query to get all admin
-                      $sql="SELECT * FROM tbl_admin";
-
-                      //execute the query
-                      $res=mysqli_query($conn , $sql);
-
-                      //check whether the query is executed or not
-                      if($res==TRUE){
-                        //COUNT rows to check whether there are data in database
-                        $count=mysqli_num_rows($res);
-
-                        $sn=1; //create a variable and assign the value
-
-                        //check number of rows
-                        if($count>0){
-                            //we have data in database
-                            while($rows=mysqli_fetch_assoc($res))
-                            {
-                                //using while loop to get all the data in database
-                                //it will run as long as we have data in database
-
-                                //get individual data
-                                $id=$rows['id'];
-                                $full_name=$rows['full_name'];
-                                $username=$rows['username'];
-
-                                //display values in table
-
-                                ?>
-
-                    <tr>
-                        <td><?php echo $sn++?></td>
-                        <td><?php echo $full_name?></td>
-                        <td><?php echo $username?></td>
-                        <td>
-                            <a href="" class="btn-secondary">Update</a>
-                            <a href="" class="btn-danger">Delete</a>
-
-                        </td>
-                    </tr>
-
-                                <?php
-                            }
-
-                        }
-
-                      }
-                    
-                    ?>
-
-                    
-                </table>
-
-            
-                
-
-            </div>
-
-        </div>
-        <!-- main content section ends -->
+        </table>
 
 
 
-        <?php include('partials/footer.php') ?>
+
+    </div>
+
+</div>
+<!-- main content section ends -->
+
+
+
+<?php include('partials/footer.php') ?>
